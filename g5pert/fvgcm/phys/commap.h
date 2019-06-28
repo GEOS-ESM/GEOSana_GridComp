69e0272f133e68d5ba586c51d516afb876d89cd9
Matthew Thompson <matthew.thompson@nasa.gov>
2019-06-28 12:30:04 -0400

    g5pert is now a @g5pert. Not needed here

--- g5pert/fvgcm/phys/commap.h
+++
@@ -1,5 +0,0 @@
-      real clat(plat)      ! model latitudes (radians)
-      real latdeg(plat)    ! model latitudes (degrees)
-
-! SJL 10/24/98
-      common/commap/clat, latdeg
--- g5pert/fvgcm/phys/parsst.h
+++
@@ -1,7 +0,0 @@
-c
-c $Id$
-c $Author$
-c
-      real tsice                  ! Freezing point of sea ice degrees C
-      parameter (tsice = -1.7999) ! Use this with new global STR sst data
- 
