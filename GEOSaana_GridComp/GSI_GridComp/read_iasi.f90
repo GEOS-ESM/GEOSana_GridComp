edb932fb2bf9e888830272ea6eca0234c1c17345
Meta Sienkiewicz <53273921+gmao-msienkie@users.noreply.github.com>
2022-11-17 11:36:22 -0500

    Delete unneeded call to closbf
    The "closbf" call used to be needed because there was an "openbf" call earlier in the program. Between CVS v1.26.2.1 and v1.26.2.2 that "openbf" call was removed but the (now unneeded) "closbf" call was not removed.  It isn't a problem with the static allocation BUFR lib but it caused a problem when the BUFR lib was compiled with DYNAMIC allocation.   Let's just get rid of this call.

--- GEOSaana_GridComp/GSI_GridComp/read_iasi.f90
+++ GEOSaana_GridComp/GSI_GridComp/read_iasi.f90
@@ -414,7 +414,6 @@ subroutine read_iasi(mype,val_iasi,ithin,isfcalc,rmesh,jsatid,gstime,&
      end if
 
 !    Open BUFR file
-     call closbf(lnbufr)
      open(lnbufr,file=trim(infile2),form='unformatted',status='old',iostat=ierr)
 
      if(ierr /= 0) cycle ears_db_loop
