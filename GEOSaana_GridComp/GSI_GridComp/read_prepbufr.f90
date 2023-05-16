af54789684c962b0f1538a5b8baf655ad61c95f0
gmao-msienkie <gmao-msienkie@users.noreply.github.com>
2023-05-16 11:36:41 -0400

    Add GOES and JMA to the QI check in the thinning Modify thinning QI check to handle missing QIFN (use a zero value)

--- GEOSaana_GridComp/GSI_GridComp/read_prepbufr.f90
+++ GEOSaana_GridComp/GSI_GridComp/read_prepbufr.f90
@@ -1971,8 +1971,9 @@ subroutine read_prepbufr(nread,ndata,nodata,infile,obstype,lunout,twindin,sis,&
                  else
                     timedif=abs(t4dv-toff)
                  endif
-                 if(kx == 243 .or. kx == 253 .or. kx ==254) then
+                 if(kx >= 253 .and. kx <= 256) then
                     call ufbint(lunin,satqc,1,1,iret,satqcstr)
+                    if (ibfms(satqc(1)) satqc(1) = 0.0
                     crit1 = timedif/r6+half + four*(one-satqc(1)/r100)*r3_33
                  else
                     crit1 = timedif/r6+half
